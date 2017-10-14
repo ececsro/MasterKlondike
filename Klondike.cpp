a8e4f12169e90aac4dc4d2c7210a162ba8618f71
ECECSRO <ECECSRO@ES00201169.ericsson.se>
2017-10-14 14:03:24 +0200

    New Klondike Class

---
+++ Klondike.cpp
@@ -0,0 +1,18 @@
+/*
+ * Klondike.cpp
+ *
+ *  Created on: 14 oct. 2017
+ *      Author: ECECSRO
+ */
+
+#include "Klondike.h"
+
+Klondike::Klondike() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Klondike::~Klondike() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Klondike.h
@@ -0,0 +1,17 @@
+/*
+ * Klondike.h
+ *
+ *  Created on: 14 oct. 2017
+ *      Author: ECECSRO
+ */
+
+#ifndef KLONDIKE_H_
+#define KLONDIKE_H_
+
+class Klondike {
+public:
+	Klondike();
+	virtual ~Klondike();
+};
+
+#endif /* KLONDIKE_H_ */
